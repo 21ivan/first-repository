4a8655a91809a8bda10214ebb1deef4ecde97ab8
21ivan <47848786+21ivan@users.noreply.github.com>
2019-02-28 00:11:40 +0200

    Add files via upload

---
+++ task.py
@@ -0,0 +1,20 @@
+a=int(input('input a '))
+b=int(input('input b '))
+if a>=b:
+    print ('max =', a, 'min =',b)
+print ('max =', b, 'min =',a)
+###############################
+num = int(input('input num'))
+if (num%2)==0:
+    print("parne")
+else:
+    print('ne parne')
+###############################
+num=int(input('input chislo - ' ))
+factorial = 1
+while num>1:
+    factorial*=num
+    num-=1
+    #print(factorial)
+print(factorial)
+################################
