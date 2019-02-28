e5a9cb1bf4bdc59fcad2a7e8d1b0fdb3be9d45a8
21ivan <47848786+21ivan@users.noreply.github.com>
2019-02-28 12:42:11 +0200

    Add files via upload

---
+++ HW1(t1).py
@@ -0,0 +1,7 @@
+name = str(input('What is your name?\n'))
+age = int(input('How old are you?\n'))
+loc = str(input('Where do you live?\n'))
+
+print('\n\nHello', name)
+print('Your age is', age)
+print('You live in', loc)
