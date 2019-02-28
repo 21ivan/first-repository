56a147debfb2edc84aa2891f85d1d9ae6326e8a0
21ivan <47848786+21ivan@users.noreply.github.com>
2019-02-28 11:46:08 +0200

    Update ClassWork.py

--- ClassWork.py
+++ ClassWork.py
@@ -2,7 +2,8 @@ a=int(input('input a '))
 b=int(input('input b '))
 if a>=b:
     print ('max =', a, 'min =',b)
-print ('max =', b, 'min =',a)
+else:
+    print ('max =', b, 'min =',a)
 ###############################
 num = int(input('input num'))
 if (num%2)==0:
