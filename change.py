82cddbb25bebfcf941c49c57d94353f37f754fb7
21ivan <47848786+21ivan@users.noreply.github.com>
2019-02-28 11:31:20 +0200

    Add files via upload

---
+++ change.py
@@ -0,0 +1,4 @@
+num1 = int(input('input first num - '))
+num2 = int(input('input second num - '))
+num1, num2 = num2, num1
+print('fisrst num after change - ', num1, '\nsecond num after change - ', num2)
