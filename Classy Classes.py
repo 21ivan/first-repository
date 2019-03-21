ee999c792cd81347ee511dc055e344ef57764b69
21ivan <47848786+21ivan@users.noreply.github.com>
2019-03-21 12:18:18 +0200

    Create Classy Classes.py

---
+++ Classy Classes.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self,name,age):
+        self.info="{}s age is {}".format(name, age)
+        self.name = name
+        self.age = age
+    def get_info(self, info):
+        return self.infos
